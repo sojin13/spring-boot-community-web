446ef2ca8b8b7afe19ad753642cab07d1de2f6aa
sojin.kim <sojin.kim@lemonhc.com>
2020-10-20 11:35:38 +0900

    CommandRunner 확인 중

--- src/main/java/com/web/BootWebApplication.java
+++ src/main/java/com/web/BootWebApplication.java
@@ -5,42 +5,47 @@ import com.web.domain.User;
 import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication implements WebMvcConfigurer {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebApplication.class, args);
+	}
 
-        SpringApplication.run(BootWebApplication.class, args);
+	@Bean
+    public CommandLineRunner testRunner() {
+	    return args -> System.out.println("Test Success");
     }
 
-    @Bean
-    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
-        return (args) -> {
-            User user = userRepository.save(User.builder()
-                .name("havi")
-                .password("test")
-                .email("havi@gmail.com")
-                .createdDate(LocalDateTime.now())
-                .build());
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
+		return args -> {
+			User user = userRepository.save(User.builder()
+					.name("havi")
+					.password("test")
+					.email("havi@gmail.com")
+					.createdDate(LocalDateTime.now())
+					.build());
 
-            IntStream.rangeClosed(1, 200).forEach(index ->
-                    boardRepository.save(Board.builder()
-                        .title("게시글" + index)
-                        .subTitle("순서" + index)
-                        .content("콘텐츠")
-                        .boardType(BoardType.free)
-                        .createdDate(LocalDateTime.now())
-                        .updatedDate(LocalDateTime.now())
-                        .user(user).build())
-            );
-        };
-    }
+			IntStream.rangeClosed(1, 200).forEach(index ->
+				boardRepository.save(Board.builder()
+                    .title("게시글"+index)
+                    .subTitle("순서"+index)
+                    .content("컨텐츠")
+                    .boardType(BoardType.free)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .user(user).build()));
+		};
+	}
 }
--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.web.controller;
 
 import com.web.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.awt.print.Pageable;
 
 @Controller
 @RequestMapping("/board")
@@ -19,16 +19,20 @@ public class BoardController {
     @Autowired
     BoardService boardService;
 
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
     @GetMapping({"", "/"})
     public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-        return "/board/form";
+        return "board/form";
     }
 
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model) {
-        model.addAttribute("board", boardService.findBoardList((org.springframework.data.domain.Pageable) pageable)); // 형변환
-        return "/board/list";
+        model.addAttribute("board", boardService.findBoardList(pageable)); // 형변환
+        return "board/list";
     }
 
 }
